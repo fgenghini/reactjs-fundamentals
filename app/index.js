a1d2b8fe935f3151ea1b56538fceeb0e87b6d593
Fernando Genghini <fernandogenghini@gmail.com>
2016-11-02 22:17:17 -0200

    this.props.children exercise

--- app/index.js
+++ app/index.js
@@ -30,10 +30,25 @@ var ProfileLink = React.createClass({
   render: function () {
     return (
       <div>
-        <a href={'https://www.github.com/' + this.props.username}>
+        <Link href={'https://www.github.com/' + this.props.username}>
           {this.props.username}
-        </a>
+        </Link>
       </div>
+    )
+  }
+});
+
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href);
+  },
+  render: function () {
+    return (
+      <span
+        style={{color: 'blue', cursor: 'pointer'}}
+        onClick={this.changeURL}>
+        {this.props.children}
+      </span>
     )
   }
 });
